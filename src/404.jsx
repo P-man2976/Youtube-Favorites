463ba9bf785314ade8c72e444eecf5fd7092768a
P-man2976 <ikmy1119924@gmail.com>
2021-12-26 03:06:54 +0900

    3 files modified, 1 added

--- src/404.jsx
+++ src/404.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
-import {
-  Box,
-  Stack,
-  IconButton,
-  Button
-} from '@mui/material';
+import React from 'react';
+import { Box, Stack, IconButton, Button } from '@mui/material';
 import ReactMarkdown from 'react-markdown';
+import { Link as RouterLink } from 'react-router-dom';
 import { Header } from './components/index';
 
 const notFoundmd = `
@@ -13,19 +9,22 @@ const notFoundmd = `
 # 404
 
 お探しのページは見つかりませんでした。移動もしくは削除された可能性があります。
-`
+`;
 
 const Notfound = () => {
   return (
     <>
       <Header />
-      <Box>
+      <Stack>
         <ReactMarkdown className="markdown">
           {notFoundmd}
         </ReactMarkdown>
-      </Box>
+        <Button variant="outlined" component={RouterLink} to="/">
+          ホームに戻る
+        </Button>
+      </Stack>
     </>
-  )
-}
+  );
+};
 
 export default Notfound;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,6 +47,7 @@ const Header = () => {
           display: 'flex',
           justifyContent: 'space-between',
           padding: '10px',
+          backgroundColor: '#fff',
         }}
       >
         <IconButton onClick={toggleDrawer(true)}>
