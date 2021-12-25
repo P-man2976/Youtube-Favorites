24b2004d351ecc45943c554309385c14f02e5bee
P-man2976 <ikmy1119924@gmail.com>
2021-12-25 16:59:28 +0900

    2 files modified

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,10 +6,6 @@ const Home = () => {
 		<>
 			<Header />
 			<Introduction />
-			<div>
-				<h1>Hello StackBlitz!</h1>
-				<p>Start editing to see some magic happen :)</p>
-			</div>
 		</>
 	);
 }
--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -13,6 +13,19 @@ const introduction = `
 
 YouTube Favoritesとは、YouTubeのお気に入りチャンネルの配信予定・アーカイブをチェックできるサービスです。
 
+## How to use
+
+### お気に入りチャンネルを登録する
+
+1. 画面上部の検索窓からチャンネルを検索
+1. ☆ボタンを押してお気に入りに登録
+
+### お気に入りチャンネル一覧を見る
+
+1. 画面上部のメニューから、「お気に入り」を選ぶ
+
+## このサイトについて
+
 `
 
 const introductionCss = css `
@@ -21,6 +34,51 @@ const introductionCss = css `
   min-height: 100vh;
   padding: 10px;
   font-family: sans-serif;
+
+  .markdown {
+    font-family: sans-serif;
+
+    h1 {
+      font-family: sans-serif;
+      font-weight: 700;
+    }
+
+    h2 {
+      font-family: sans-serif;
+      font-weight: 700;
+      border-radius: 4px;
+      background-color: #dd0000;
+      color: white;
+      padding: 10px 15px;
+    }
+
+    h3 {
+      font-family: sans-serif;
+      border-bottom: 3px solid #cc0000;
+    }
+
+    h4 {
+      font-family: sans-serif;
+    }
+
+    h5 {
+      font-family: sans-serif;
+    }
+
+    p {
+      font-family: sans-serif;
+    }
+
+    ul, ol {
+      padding: 0 0 0.5em 1.5em;
+    }
+    
+    ul > li,
+    ol > li {
+      position: relative;
+      margin: 5px 0;
+    }
+  }
 `
 const markdownCss = css `
   font-family: sans-serif;
@@ -30,7 +88,9 @@ const Introduction = () => {
   return (
     <>
       <Box css={introductionCss}>
-        <ReactMarkdown children={introduction} css={markdownCss} />
+          <ReactMarkdown className="markdown">
+            {introduction}
+          </ReactMarkdown>
       </Box>
     </>
   )
