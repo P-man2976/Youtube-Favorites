58f39da205ba9515dd0cbb1145a058e2526bafb1
P-man2976 <ikmy1119924@gmail.com>
2021-12-25 15:27:05 +0900

    3 files modified

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Header } from "./components/index";
+import { Header, Introduction } from "./components/index";
 
 const Home = () => {
 	return (
 		<>
 			<Header />
+			<Introduction />
 			<div>
 				<h1>Hello StackBlitz!</h1>
 				<p>Start editing to see some magic happen :)</p>
--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,2 +1,14 @@
 import React from 'react';
+import { Box } from '@mui/material';
 
+const Introduction = () => {
+  return (
+    <>
+      <Box sx={{ backgroundColor: "#f9f9f9", width: "100vw", minHeight: "100vh" }}>
+
+      </Box>
+    </>
+  )
+}
+
+export default Introduction;
