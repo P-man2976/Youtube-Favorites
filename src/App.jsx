77257b486ec2182c9eeab89d44ab7fd6da64f591
P-man2976 <ikmy1119924@gmail.com>
2021-12-26 02:46:29 +0900

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home, Notfound } from './indexfiles';
-import "./style.css";
+import './style.css';
 
 export default function App() {
   return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="channels" element={<Home />} />
-				<Route path=":id" element={<Home />} />
-				<Route path="*" element={<Home />} />
-				<Route index element={<Home />} />
-			<Route path="favorites" element={<Home />} />
-			<Route path="settings" element={<Home />} />
-			<Route path="*" element={<Notfound />} />
-		</Routes>
+    <Routes>
+      <Route path="/" element={<Notfound />} />
+      	<Route path="channels" element={<Notfound />} />
+      		<Route path=":id" element={<Notfound />} />
+      		<Route path="*" element={<Notfound />} />
+      		<Route index element={<Home />} />
+      	<Route path="favorites" element={<Home />} />
+      	<Route path="settings" element={<Home />} />
+      	<Route path="*" element={<Notfound />} />
+    </Routes>
   );
 }
