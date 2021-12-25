cda36d3a594e85ff0a22616c1b7b74467556509f
P-man2976 <ikmy1119924@gmail.com>
2021-12-25 21:27:33 +0900

    1 file modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,48 +1,20 @@
 /**@jsx jsx */
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   Stack,
   InputBase,
   TextField,
   IconButton,
-  Divider,
+  SwipeableDrawer,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import { styled, alpha } from '@mui/material/styles';
 import Stack from '@mui/material/Stack/Stack';
 import { jsx, css } from '@emotion/react';
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
-
 const Header = () => {
+  
+
   return (
     <>
       <Box sx={{padding: "10px"}}>
